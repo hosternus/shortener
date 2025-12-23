304fbe61d2b9e6703232c192a1e4d10f6670e244
hosternus <90009071+hosternus@users.noreply.github.com>
2025-12-24 01:51:09 +0300

    short id len limited by 6

--- database/model.py
+++ database/model.py
@@ -11,7 +11,7 @@ class Url(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     source_url: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
-    short_id: Mapped[str] = mapped_column(String(8), nullable=False, unique=True)
+    short_id: Mapped[str] = mapped_column(String(6), nullable=False, unique=True)
     visits: Mapped[int] = mapped_column(nullable=False, default=0, server_default="0")
     created_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now, server_default=func.now()) # pylint: disable=not-callable
     updated_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now, server_default=func.now(), onupdate=func.now(), server_onupdate=func.now()) # pylint: disable=not-callable
--- utils.py
+++ utils.py
@@ -2,4 +2,4 @@ import random
 import string
 
 def generate_short_id() -> str:
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
